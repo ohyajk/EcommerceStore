5da7ff38380bf7faead6e8627c3666a29d123b4c
ohyajk <ohyajk@gmail.com>
2023-02-24 13:01:38 +0530

    Add CDN JS

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
             <nav className='md:flex justify-between'>
                 <div className='flex justify-between items-center px-4 md:px-0' >
                     <h1 className='text-2xl font-[cursive]'>Ecom App</h1>
-                    <button className={menu ? 'hidden' : 'md:hidden'} onClick={() => { setMenu(true) }} >HAM</button>
+                    <button className={menu ? 'hidden' : 'md:hidden'} onClick={() => { setMenu(true) }} ><i class="fa-solid fa-bars"></i></button>
                     <button className={menu ? 'flex md:hidden' : 'hidden'} onClick={() => { setMenu(false) }} >X</button>
                 </div>
                 <ul className={menu ? 'py-6 md:py-0 absolute md:relative w-full bg-pink-600/70 backdrop-blur-md flex flex-col md:flex-row justify-center items-center gap-4' : 'hidden md:flex flex-col md:flex-row justify-center items-center gap-4'}>

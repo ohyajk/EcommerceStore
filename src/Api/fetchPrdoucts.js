7b7864bf40fda6bafbd0958dfccb87f002af30e6
ohyajk <ohyajk@gmail.com>
2023-03-26 13:21:58 +0530

    Remove fetch

--- src/Api/fetchPrdoucts.js
+++
@@ -1,8 +0,0 @@
-const fetchProducts = async() => {
-    const products = await fetch('https://fakestoreapi.com/products')
-    const data = await products.json()
-    return data
-}
-
-
-export default fetchProducts
--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,20 +1,4 @@
-import React,{ useState, useEffect } from "react";
-import fetchProducts from "../Api/FetchPrdoucts";
-
 const HomePage = () => {
-
-const [products, setProducts] = useState()
-
-const fetcher = async() => {
-  const fetch = await fetchProducts()
-  setProducts(fetch)
-}
-
-useEffect(() => {
-  fetcher()
-},[])
-
-console.log(products)
 
   return (
     <div className="text-blackecom">
